92240c224eb71d74eae94238b84ca7bbd17e46a7
Ross Bratton <ross.bratton@yahoo.co.uk>
2023-08-05 20:03:37 +0100

    Updated dark mode toggle to look nicer

--- src/app/core/navigation/navigation.component.ts
+++ src/app/core/navigation/navigation.component.ts
@@ -1,7 +1,8 @@
 import { DOCUMENT } from '@angular/common';
-import { Component, Inject } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { AfterViewInit, Component, ElementRef, Inject, ViewChild } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
 import { LocalstorageService } from 'src/app/services/localstorage.service';
-
 
 enum Mode { LIGHT = 'light', DARK = 'dark' };
 
@@ -10,14 +11,21 @@ enum Mode { LIGHT = 'light', DARK = 'dark' };
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.scss']
 })
-export class NavigationComponent {
+export class NavigationComponent implements AfterViewInit {
+  @ViewChild('darkModeToggle', { read: ElementRef }) element: ElementRef | undefined;
+
   currentMode = Mode.LIGHT;
 
-  constructor(@Inject(DOCUMENT) private document: Document, private localstorageService: LocalstorageService<Mode>) {
+  constructor(@Inject(DOCUMENT) private document: Document, private http: HttpClient, private localstorageService: LocalstorageService<Mode>) {
     this.currentMode = localstorageService.get('theme') || Mode.LIGHT;
     this.document.body.classList.add(this.currentMode);
   }
 
+  ngAfterViewInit() {
+    this.setIcon();
+  }
+
+  // Toggles the bodys class based on the current theme.
   toggleTheme() {
     this.document.body.classList.remove(this.currentMode);
 
@@ -29,6 +37,29 @@ export class NavigationComponent {
 
     this.localstorageService.save('theme', this.currentMode);
     this.document.body.classList.add(this.currentMode);
+  }
+
+  async setIcon() {
+    const headers = new HttpHeaders();
+    headers.set('Accept', 'image/svg+xml');
+
+    const $darkMode = this.http.get('assets/icons/dark-mode.svg', { headers, responseType: 'text' });
+    const $lightMode = this.http.get('assets/icons/light-mode.svg', { headers, responseType: 'text' });
+
+    const darkModeRes: string = await lastValueFrom($darkMode);
+    const lightModeRes: string = await lastValueFrom($lightMode);
+
+    if (this.element) {
+      this.element.nativeElement.querySelector('.mdc-switch__icon--on').firstChild.setAttribute('d', this.getPathD(darkModeRes));
+      this.element.nativeElement.querySelector('.mdc-switch__icon--off').firstChild.setAttribute('d', this.getPathD(lightModeRes));
+    }
+  }
+
+  getPathD(value: string): string {
+    const path: string = value.split('" d="')[1];
+    const pathD: string = path.split('"')[0];
+
+    return pathD;
   }
 
   isDarkMode() {
