0d5cc2a93793c20bde91ef1a52b8db9e458d322b
Ross Bratton <rossbratton20@gmail.com>
2024-04-13 19:57:36 +0100

    Updated MangaModel to ensure that data gets assigned correctly

--- src/app/models/manga.model.ts
+++ src/app/models/manga.model.ts
@@ -3,22 +3,33 @@ import { DateTime } from 'luxon';
 export class Manga {
   id: string;
   name: string;
+  authorId: string;
   imageUrl: string;
   releaseDate: string = DateTime.now().toFormat('yyyy-MM-dd');
   finishedDate: string = DateTime.now().toFormat('yyyy-MM-dd');
 
   constructor(data: Manga) {
-    Object.assign(this, data);
+    this.id = data.id;
+    this.name = data.name;
+    this.authorId = data.authorId;
+    this.imageUrl = data.imageUrl;
+    this.releaseDate = data.releaseDate;
+    this.finishedDate = data.finishedDate;
   }
 }
 
 export class CreateManga {
   name: string;
   imageUrl: string;
+  authorId: string;
   releaseDate: string = DateTime.now().toFormat('yyyy-MM-dd');
   finishedDate: string = DateTime.now().toFormat('yyyy-MM-dd');
 
   constructor(data: CreateManga) {
-    Object.assign(this, data);
+    this.name = data.name;
+    this.imageUrl = data.imageUrl;
+    this.authorId = data.authorId;
+    this.releaseDate = data.releaseDate;
+    this.finishedDate = data.finishedDate;
   }
 }
